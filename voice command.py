e0fc14fc60c9cfd3dfa05dc53d772e822c80cb0b
Melanee <74653444+Melanee-Melanee@users.noreply.github.com>
2022-04-16 12:04:25 +0430

    Add files via upload

---
+++ voice command.py
@@ -0,0 +1,12 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+engine.say('Hi, This is Melanee\'s voice')
+engine.say('I am a python, data science programmer')
+engine.say('I have a complicated personality')
+engine.say('and have remarkable talent in information gathering')
+engine.say('So please pay attention to your privacy when you are contacting with me!')
+engine.say('Be Careful and goodbye!')
+
+engine.runAndWait()
+
