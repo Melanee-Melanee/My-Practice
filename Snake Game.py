b704bb4c6dafec9e9dc4257cca719444f6deba4f
Shaghyegh Rahmani <74653444+Melanee-Melanee@users.noreply.github.com>
2026-02-22 16:28:24 +0330

    Update Snake Game.py

--- Snake Game.py
+++ Snake Game.py
@@ -12,7 +12,7 @@ import pygame, sys, time, random
 # Hard      ->  40
 # Harder    ->  60
 # Impossible->  120
-difficulty = 25
+difficulty = 10
 
 # Window size
 frame_size_x = 720
@@ -167,4 +167,5 @@ while True:
     # Refresh game screen
     pygame.display.update()
     # Refresh rate
+
     fps_controller.tick(difficulty)
